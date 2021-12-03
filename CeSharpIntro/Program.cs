8f3d0f6c845011a8f216ac8aabab46878bce86a3
Sylwia Łuczak-Jagieła <sylwia.luczak-jagiela@kainos.com>
2021-12-03 10:04:01 +0100

    Add project files.

---
+++ CeSharpIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CeSharpIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
